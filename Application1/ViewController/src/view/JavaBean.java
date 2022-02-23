c0009d234ca634644e9c75b57402b7b697a66d50
mfayed70 <mohamed.fayed@gmail.com>
2022-02-23 20:10:48 +0200

    Add files via upload

---
+++ Application1/ViewController/src/view/JavaBean.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.faces.event.ActionEvent;
+
+public class JavaBean {
+    public JavaBean() {
+    }
+
+    public void openPopup(ActionEvent actionEvent) {
+        // Add event code here...
+        
+    }
+}
